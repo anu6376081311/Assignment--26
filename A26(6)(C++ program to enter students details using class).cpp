e1357c0c2986727f7625b3bea9f7d3f0d339d305
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-26 15:50:44 +0530

    Add files via upload

---
+++ A26(6)(C++ program to enter students details using class).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+  private:
+     int marks;
+     char grade;
+ public:
+     Student(int m,char g)
+     {
+        marks = m;
+        grade = g;
+     }
+
+     void show()
+     {
+        cout<<"\nMarks ="<<marks<<endl;
+         cout<<"\nGrade ="<<grade<<endl;
+     }
+};
+
+int main()
+{
+    Student s1(730,'A'),s2(621,'B');
+    cout<<"\nRecord of student 1 :: --------------"<<endl;
+    s1.show();
+     cout<<"\nRecord of student 2 :: --------------"<<endl;
+    s2.show();
+
+    return 0;
+    
+}
